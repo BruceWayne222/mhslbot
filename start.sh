3596ff5bdf18417fda31f2ac6bb1b058318ae25c
BruceWayne222 <77908276+BruceWayne222@users.noreply.github.com>
2021-12-23 16:23:22 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,7 @@
+echo "Cloning Repo, Please Wait..."
+git clone -b master https://github.com/BruceWayne222/tgmoviebot /tgmoviebot
+cd /tgmoviebot
+echo "Installing Requirements..."
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
+python3 bot.py
