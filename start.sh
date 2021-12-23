8c18834e1b4335d86f98c1cd59cca76df01d5b93
BruceWayne222 <77908276+BruceWayne222@users.noreply.github.com>
2021-12-23 16:41:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/BruceWayne222/tgmoviebot /tgmoviebot
-cd /tgmoviebot
+git clone -b main https://github.com/BruceWayne222/mhslbot /mhslbot
+cd /mhslbot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
